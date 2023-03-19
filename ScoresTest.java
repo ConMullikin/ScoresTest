1547ecb58df556d8053c8a0307fec1155e6e1fdd
ConMullikin <mullikincp22@gcc.edu>
2023-03-19 17:15:18 -0400

    Starter

--- ScoresTest.java
+++ ScoresTest.java
@@ -57,8 +57,21 @@ public class ScoresTest {
         });
     }
 
-    @Test public void validMax() {
-        ScoreZ scores = new ScoreZ("2 4 16 32");
-        assertEquals(32,scores.getMax());
+    @Test public void maxTestBeginning() {
+        ScoreZ scores = new ScoreZ("120000 32 74 -23 65");
+        int maxValue = scores.getMax();
+        assertEquals(120000,scores.get(0));
+    }
+
+    @Test public void maxTestMiddle() {
+        ScoreZ scores = new ScoreZ("120000 32 74 -23 65");
+        int maxValue = scores.getMax();
+        assertEquals(74,scores.get(2));
+    }
+
+    @Test public void maxTestEnd() {
+        ScoreZ scores = new ScoreZ("120000 32 74 -23 65");
+        int maxValue = scores.getMax();
+        assertEquals(65,scores.get(4));
     }
 }
