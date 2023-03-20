7fe2d3faeb0ebe875815777d5a9b419be26f7e6d
matthew-hase-00 <HaseMG22@gcc.edu>
2023-03-20 02:28:32 -0400

    Renamed "Scores" class to "ScoreZ" class to allow for test class functionality. Initial commits reflect first grouping of tests for email submission to Dr. Dickinson

--- ScoreZ.java
+++ ScoreZ.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
-public class Scores {
+public class ScoreZ {
     private final ArrayList<Integer> nums;
 
     /**
@@ -10,24 +10,38 @@ public class Scores {
      *                Each integer is a score. This class holds the list of scores parsed from the String.
      * @throws IllegalArgumentException if numbers is not a whitespace-delimited list of zero or more integers.
      */
-    public Scores(String numbers) throws IllegalArgumentException {
-        nums = new ArrayList<>();
+    public ScoreZ(String numbers) throws IllegalArgumentException {
+       Scanner inScan = new Scanner(numbers);
+       nums = new ArrayList<>();
+
+       while(inScan.hasNext() && inScan.hasNextLine()) {
+           nums.add(inScan.nextInt());
+       }
     }
 
     /**
      * @return the number of scores in this object i.e. the number of integers in the String given to the constructor
      */
-    public int getNumScores() { return -1; }
+    public int getNumScores() { return nums.size(); }
 
     /**
      * @param index An integer index in the range [0, getNumScores())
      * @return The score at the specified index. For example, the first score in the String is at index 0.
      */
-    public int get(int index) { return -1; }
+    public int get(int index) { return nums.get(index); }
 
     /**
      * @return the maximum score stored in this object
-     * @throws java.util.NoSuchElementException if there are no scores
+     * @throws NoSuchElementException if there are no scores
      */
-    public int getMax() throws NoSuchElementException { return -1; }
+    public int getMax() throws NoSuchElementException {
+        int max = nums.get(0);
+
+        for (int i = 0; i < nums.size(); i++) {
+            if(nums.get(i) > max) {
+                max = nums.get(i);
+            }
+        }
+        return max;
+    }
 }
