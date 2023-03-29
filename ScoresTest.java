604ea4134f4b214b9ce9afc6c47b4796fda726af
matthew-hase-00 <HaseMG22@gcc.edu>
2023-03-29 14:52:34 -0400

    Minorly revised, reorganized, and Javadoc'd code

--- ScoresTest.java
+++ ScoresTest.java
@@ -141,87 +141,11 @@ public class ScoresTest {
        Scores scores = new Scores(numbers);
        assertEquals(9, scores.getNumScores());
     }
-    
-    /*
-     * getMax() tests
+
+    /**
+     * Tests to see if an integer larger than the possible maximum integer can be passed through the Scores constructor
+     * and added to the nums ArrayList
      */
-
-    @Test public void emptyMax() {
-        Scores scores = new Scores("");
-        assertThrows(NoSuchElementException.class, () -> {
-            scores.getMax();
-        });
-    }
-
-    @Test public void maxTestMaxInteger() {
-
-    }
-
-    @Test public void maxTestBeginning() {
-        Scores scores = new Scores("120000 32 74 -23 65");
-        int maxValue = scores.getMax();
-        assertEquals(120000,scores.get(0));
-    }
-
-    @Test public void maxTestMiddle() {
-        Scores scores = new Scores("-20 74 32 -23 65");
-        int maxValue = scores.getMax();
-        assertEquals(74, maxValue);
-    }
-
-    @Test public void maxTestMiddle2() {
-        Scores scores = new Scores("30 11 89 -9 20");
-        int maxValue = scores.getMax();
-        assertEquals(89, maxValue);
-    }
-
-    @Test public void maxTestMiddle3() {
-        Scores scores = new Scores("190321 894012 -86432 1297893 -0732");
-        int maxValues = scores.getMax();
-        assertEquals(1297893, maxValues);
-    }
-
-    @Test public void maxTestEnd() {
-        Scores scores = new Scores("65 32 74 -23 120000");
-        int maxValue = scores.getMax();
-        assertEquals(120000, maxValue);
-    }
-
-    @Test public void maxTestSingleScore() {
-        String numbers = "-9999999";
-        Scores newScores = new Scores(numbers);
-        int max = newScores.getMax();
-        assertEquals(-9999999, max);
-    }
-
-    @Test public void maxTestNewLine() {
-        Scores scores = new Scores("1 2 3 \n 4 5");
-        int maxValue = scores.getMax();
-        assertEquals(5, maxValue);
-    }
-
-    @Test public void maxTestNewLineNWS() {
-        String numbers = "1 2 3 4 5" +
-                "6 7 8 9 10";
-        Scores scores = new Scores(numbers);
-        int maxValue = scores.getMax();
-        assertEquals(56, maxValue);
-    }
-
-    @Test public void getMaxTestMaxInteger() {
-        String numbers = "3 5 -0 87 2147483647 -60 -97 -121 103";
-        Scores newScores = new Scores(numbers);
-        int max = newScores.getMax();
-        assertEquals(Integer.MAX_VALUE, max);
-    }
-
-    @Test public void getMaxTestMinInteger() {
-        String numbers = "-2147483648";
-        Scores newScores = new Scores(numbers);
-        int max = newScores.getMax();
-        assertEquals(Integer.MIN_VALUE, max);
-    }
-
     @Test public void OOBInteger() {
         String numbers = "2147483648";
         assertThrows(IllegalArgumentException.class, () -> {
@@ -229,10 +153,132 @@ public class ScoresTest {
         });
     }
 
+    /**
+     * Tests to see if an integer smaller than the possible minimum integer can be passed through the Scores constructor
+     * and added to the nums ArrayList
+     */
     @Test public void OOBInteger2() {
         String numbers = "-2147483649";
         assertThrows(IllegalArgumentException.class, () -> {
             Scores newScores = new Scores(numbers);
         });
     }
+    
+    /*
+     * getMax() tests
+     */
+
+    /**
+     * Establishes a new Scores class with an empty String argument and attempts to retrieve the maximum value from the nums ArrayList
+     */
+    @Test public void emptyMax() {
+        Scores scores = new Scores("");
+        assertThrows(NoSuchElementException.class, () -> {
+            scores.getMax();
+        });
+    }
+
+    /**
+     * Establishes a new Scores class and invokes the getMax() method to ensure that the maximum is located at the 0th index
+     * of the nums ArrayList
+     */
+    @Test public void maxTestBeginning() {
+        Scores scores = new Scores("120000 32 74 -23 65");
+        int maxValue = scores.getMax();
+        assertEquals(120000, maxValue);
+    }
+
+    /**
+     * Establishes a new Scores class with positive and negative values in the String and invokes the getMax() method to ensure
+     * the proper value is found to be 74 (at index 1)
+     */
+    @Test public void maxTestMiddle() {
+        Scores scores = new Scores("-20 74 32 -23 65");
+        int maxValue = scores.getMax();
+        assertEquals(scores.get(1), maxValue);
+    }
+
+    /**
+     * Similar to above test, except that maxValue should correspond to the value located at index 2 in the ArrayList
+     */
+    @Test public void maxTestMiddle2() {
+        Scores scores = new Scores("30 11 89 -9 20");
+        int maxValue = scores.getMax();
+        assertEquals(scores.get(2), maxValue);
+    }
+
+    /**
+     * Similar to above tests, except with numbers of higher absolute value, and the max value should correspond to the
+     * value at index 3 in the ArrayList
+     */
+    @Test public void maxTestMiddle3() {
+        Scores scores = new Scores("190321 894012 -86432 1297893 -0732");
+        int maxValues = scores.getMax();
+        assertEquals(scores.get(3), maxValues);
+    }
+
+    /**
+     * Establishes a new Scores class and invokes the getMax() method to ensure that the max value occurs at the
+     * last index of the nums ArrayList
+     */
+    @Test public void maxTestEnd() {
+        Scores scores = new Scores("65 32 74 -23 120000");
+        int maxValue = scores.getMax();
+        assertEquals(scores.get(scores.getNumScores()-1), maxValue);
+    }
+
+    /**
+     * Tests the getMax() method to see if it returns a single negative value
+     */
+    @Test public void maxTestSingleScore() {
+        String numbers = "-9999999";
+        Scores newScores = new Scores(numbers);
+        int max = newScores.getMax();
+        assertEquals(-9999999, max);
+    }
+
+    /**
+     * Tests the getMax() method to ensure that there are no errors when passing newline characters in the
+     * String argument over to the ArrayList
+     */
+    @Test public void maxTestNewLine() {
+        Scores scores = new Scores("1 2 3 \n 4 5");
+        int maxValue = scores.getMax();
+        assertEquals(5, maxValue);
+    }
+
+    /**
+     * Tests to see if getMax() returns the proper value for a broken String argument in the Scores constructor
+     * with no whitespaces (i.e. a [5" + "6] should equate to an integer of 56 added to the nums ArrayList)
+     */
+    @Test public void maxTestBrokenStringNWS() {
+        String numbers = "1 2 3 4 5" +
+                "6 7 8 9 10";
+        Scores scores = new Scores(numbers);
+        int maxValue = scores.getMax();
+        assertEquals(56, maxValue);
+    }
+
+    /**
+     * Tests to see if a String argument containing the maximum possible integer value can have it be caught
+     * by the getMax() method
+     */
+    @Test public void getMaxTestMaxInteger() {
+        String numbers = "3 5 -0 87 2147483647 -60 -97 -121 103";
+        Scores newScores = new Scores(numbers);
+        int max = newScores.getMax();
+        assertEquals(Integer.MAX_VALUE, max);
+    }
+
+    /**
+     * Tests to see if a String argument containing the minimum possible integer value can have it be caught by
+     * the getMax() method
+     */
+    @Test public void getMaxTestMinInteger() {
+        String numbers = "-2147483648";
+        Scores newScores = new Scores(numbers);
+        int max = newScores.getMax();
+        assertEquals(Integer.MIN_VALUE, max);
+    }
+
 }
